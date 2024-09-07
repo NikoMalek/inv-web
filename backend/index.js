fd991ef33451b3ba37421e0fc66c0de0a4672675
NikoMalek <114967158+NikoMalek@users.noreply.github.com>
2024-09-07 12:14:29 -0400

    Implementa lógica de registro y login con db-local en Node.js
    Se implementa la funcionalidad de registro y login con una base de datos local en Node.js. Las contraseñas de los usuarios se encriptan utilizando bcrypt para garantizar su seguridad.

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import { PORT } from './config.js'
+import { UserDB } from './user-db.js'
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  console.log(req.body)
+  try {
+    const user = await UserDB.login({ username, password })
+    res.send(user)
+  } catch (error) {
+    res.status(401).send(error.message)
+  }
+})
+
+
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body
+  console.log(req.body)
+
+  try{
+    const id = await UserDB.create({ username, password })
+    res.send({ id })
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+})
+app.get('/profile', (req, res) => {})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
---
+++ backend/user-db.js
@@ -0,0 +1,75 @@
+import DBLocal from 'db-local';
+import cypto from 'crypto';
+import bcrypt from 'bcrypt';
+import { z } from 'zod';
+
+const { Schema } = new DBLocal({ path: './userdb' });
+
+// Aqui se define el esquema de usuario
+const userSchema = z.object({
+  username: z.string().min(1, 'username must be a non-empty string'),
+  password: z.string().min(3, 'password must be at least 3 characters long')
+});
+
+
+const User = Schema('User', {
+  _id: {type: String,required: true},
+  username: {type: String,required: true},
+  password: {type: String,required: true}
+})
+
+export class UserDB {
+  static async create ({ username , password }) {
+
+    try {
+      userSchema.parse({ username, password });
+    } catch (error) {
+      throw new Error(error.errors[0].message);
+    }
+
+
+    // verificar si el usuario no existe
+
+    const user = User.findOne ({ username })
+    if (user) {
+      throw new Error('username already exists')
+    }
+
+    // crear id
+    const id = crypto.randomUUID()
+
+    const hashedPassword = await bcrypt.hash(password, 10) // 10 es el número de veces que se va a encriptar la contraseña
+
+    User.create({
+      _id: id,
+      username, 
+      password: hashedPassword
+    }).save()
+    
+    return id
+
+  }
+  static async login ({ username , password }) {
+
+    try {
+      userSchema.parse({ username, password });
+    } catch (error) {
+      throw new Error(error.errors[0].message);
+    }
+
+    const user = User.findOne({ username })
+    if (!user) { throw new Error('username does not exist') }
+
+    const isValidPassword = await bcrypt.compare(password, user.password)
+    if (!isValidPassword) { throw new Error('invalid password') }
+
+    const { password: _, ...publicUser } = user
+    // const { _id,password: _, ...publicUser } = user
+
+    return publicUser
+
+  }
+
+
+
+}
