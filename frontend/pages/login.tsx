56af5a5cee88bc113d44f33961a9ea07744a6bd4
NikoMalek <114967158+NikoMalek@users.noreply.github.com>
2024-09-07 12:12:44 -0400

    Implementación de pagina login en el frontend

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { Form } from '../components/form';
+// import { signIn } from 'app/auth';
+import { SubmitButton } from '../components/submit-button';
+import '../app/globals.css'
+
+
+
+
+export default function Login() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-50 dark:bg-gray-900">
+    <div className="absolute top-4 right-4">
+      <button
+        onClick={toggleDarkMode}
+        className="p-2 bg-gray-200 dark:bg-gray-700 rounded-full"
+      >
+        {isDarkMode ? '🌞' : '🌜'}
+      </button>
+    </div>
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
+      <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
+        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
+          <h3 className="text-xl font-semibold">Iniciar sesión</h3>
+          <p className="text-sm text-gray-500">
+          Usa tu correo electrónico y contraseña para iniciar sesión
+          </p>
+        </div>
+        <Form
+          action={async (formData: FormData) => {
+            // 'use server';
+
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+
+            // await signIn('credentials', {
+            //   redirectTo: '/protected',
+            //   email: formData.get('email') as string,
+            //   password: formData.get('password') as string,
+            // });
+          }}
+        >
+          <SubmitButton>Iniciar sesión</SubmitButton>
+          <p className="text-center text-sm text-gray-600">
+            {"¿No tienes una cuenta? "}
+            <Link href="/register" className="font-semibold text-gray-800">
+            Regístrate
+            </Link>
+            {' gratis (100 dolares).'}
+          </p>
+        </Form>
+      </div>
+    </div>
+  </div>
+  );
+}
