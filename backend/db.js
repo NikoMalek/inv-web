19ff966e814554dd99b83cfc4de76c5b40759df3
NikoMalek <114967158+NikoMalek@users.noreply.github.com>
2024-09-29 00:41:48 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,9 +5,9 @@ dotenv.config();
 
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.MYSQL_HOST,
+  host: process.env.DB_HOST,
   dialect: 'postgres',
-  port: process.env.MYSQL_PORT,
+  port: process.env.DB_PORT,
   logging: false, // Deshabilita los logs de las consultas SQL
 
 });
