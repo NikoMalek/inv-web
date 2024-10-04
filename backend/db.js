858beec28e20b370749320afe6af09261f06b5b0
Guz <106195366+Guztavo21@users.noreply.github.com>
2024-10-03 23:05:37 -0300

    CAMBIO DE VERSION DE REACT/ INSTALACIO SHADC-UI
    si descargas estos cambios te dejara de funcionar varias paginas por que hay que  cambiar los componentes que tenemos a los de la librería.

--- backend/db.js
+++ backend/db.js
@@ -5,17 +5,24 @@ dotenv.config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  port: process.env.DB_PORT,
-  logging: false, // Deshabilita los logs de las consultas SQL
-  dialectOptions: isProduction ? {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  } : {}
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    logging: false, // Disable SQL query logs
+    dialectOptions: isProduction
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      : {}
+  }
+);
 
-});
 export default sequelize;
