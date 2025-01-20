f0afe66822b86044d615923801f6664fe88abe3f
Nicolás Aleuanlli <114967158+NikoMalek@users.noreply.github.com>
2025-01-20 14:24:23 -0300

    Solución bug de id_empresa

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,9 +8,10 @@ const router = express.Router();
 
 router.post('/', async (req, res) => {
   try {
-    const id_empresa = req.user?.idEmpresa;
-    const { nombre, description, imagen, codigoBarras, precio, cantidad, ultima_actualizacion, idReponedor } = req.body;
-    const id = await ProductDB.create({ nombre, description, imagen, codigoBarras, id_empresa, precio, cantidad, ultima_actualizacion, idReponedor });
+    const id_empresa = req.body.id_empresa || req.user?.idEmpresa;
+    console.log(req.body)
+    const { nombre, description, imagen, codigoBarras, precio, cantidad, ultima_actualizacion, id_reponedor } = req.body;
+    const id = await ProductDB.create({ nombre, description, imagen, codigoBarras, id_empresa, precio, cantidad, ultima_actualizacion, id_reponedor });
     res.json({ id });
   } catch (error) {
     res.status(400).send(error.message);
